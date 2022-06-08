7dfc897a3b40d545d8fb309d1fecd847b26bbbaa
Yassin-hagenimana <hyassin509@gmail.com>
2022-06-09 00:12:18 +0200

    footer

--- autotyping.js
+++ autotyping.js
@@ -119,7 +119,7 @@ var AutoTyping = (function (e) {
 ]).default;
 
 
-const exampleText = ['Developer', 'Designer', 'Engineer'];
+const exampleText = ['Developer', 'Designer', 'Engineer','Tester','Analyst'];
 const exampleTyping = new AutoTyping('#text', exampleText, {
     typeSpeed: 100,
     deleteSpeed: 100,
